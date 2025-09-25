bdc5163e2155171e7db9332ba302c89ee9f5c7bf
MatWaze <coolmartun@gmail.com>
2025-09-25 13:46:50 +0400

    models moved

--- api/services/workerService.ts
+++ api/services/workerService.ts
@@ -1,45 +1,5 @@
 import { workerData } from 'worker_threads';
-
-export interface GameResult
-{
-	players: Array<{
-		id: string;
-		username: string;
-		place: string;
-		playersKicked: number;
-		isActive: boolean;
-		score: number
-	}>;
-	state: 'finished' | 'aborted';
-	fee: number
-}
-
-export interface GamePlayer
-{
-	id: string;
-	username: string;
-	position: number;
-	x: number;
-	y: number;
-	isActive: boolean;
-	place: string,
-	playersKicked: number
-	prevX?: number,
-	prevY?: number;
-	velocityX?: number;
-	velocityY?: number;
-}
-
-export interface GameState
-{
-	roomId: string;
-	state: 'countdown' | 'playing' | 'finished';
-	ballPosition: [number, number];
-	ballVelocity: [number, number];
-	players: GamePlayer[];
-	countdownSeconds: number;
-	whoHitTheBall: GamePlayer | undefined
-}
+import { GamePlayer, GameResult, GameState } from '../models/gameModels';
 
 const PADDLE_SIDE_PERCENT = 0.1;
 
