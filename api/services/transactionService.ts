50f2c629c69ab1c0874793b205ab47fb0d0e792b
MatWaze <coolmartun@gmail.com>
2025-11-07 17:01:38 +0400

    depositBlob: only update withdrawable balance for game reward payFee: update withdrawable balance so that it's correctly calculated getUsersTransactions: get transactions of type WITHDRAWAL and DEPOSIT up to previous week

--- api/services/transactionService.ts
+++ api/services/transactionService.ts
@@ -31,7 +31,13 @@ export async function getUsersTransactions(userId: string)
 {
 	try
 	{
-		return await transactionRepo.getUsersTransactionsAsync(userId);
+		const lastWeekDate = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+
+		return from(await transactionRepo.getUsersTransactionsAsync(userId))
+			.whereAsync(async t =>
+				(t.type === TransactionType.DEPOSIT || t.type === TransactionType.WITHDRAWAL) &&
+				t.createdAt >= lastWeekDate
+			);
 	}
 	catch (e)
 	{
@@ -54,17 +60,19 @@ export async function createTransaction(
 	{
 		type,
 		amount,
-		userId
+		userId,
+		status
 	} :
 	{
-		type: string
+		type: TransactionType
 		amount: number,
-		userId: string
+		userId: string,
+		status: TransactionStatus
 	})
 {
 	try
 	{
-		await transactionRepo.createTransactionAsync({ type, amount, userId });
+		await transactionRepo.createTransactionAsync({ type, amount, userId, status });
 	}
 	catch (e)
 	{
@@ -72,7 +80,7 @@ export async function createTransaction(
 	}
 }
 
-export async function depositBonus(user: any)
+export async function depositBonusBlob(user: any)
 {
 	try
 	{
@@ -82,10 +90,11 @@ export async function depositBonus(user: any)
 		{
 			type: TransactionType.BONUS,
 			amount: bonusAmount,
-			userId: user.Id
+			userId: user.Id,
+			status: TransactionStatus.SUCCESS
 		});
 	
-		updateBalance(user, bonusAmount);	
+		updateBalance(user, bonusAmount);
 	}
 	catch (e)
 	{
@@ -93,7 +102,11 @@ export async function depositBonus(user: any)
 	}
 }
 
-export async function depositWonBlob(user: any, amount: number)
+export async function depositBlob(
+	user: any,
+	amount: number,
+	withdrawalBalanceUpdate = false
+)
 {
 	try
 	{
@@ -101,11 +114,14 @@ export async function depositWonBlob(user: any, amount: number)
 		{
 			type: TransactionType.DEPOSIT,
 			amount: amount,
-			userId: user.Id
+			userId: user.Id,
+			status: TransactionStatus.SUCCESS
 		});
 	
 		updateBalance(user, user.balance + amount);
-		updateWithdrawabalBalance(user, user.withdrawAmount + amount);
+
+		if (withdrawalBalanceUpdate)
+			updateWithdrawabalBalance(user, user.withdrawAmount + amount);
 	}
 	catch (e)
 	{
@@ -124,7 +140,8 @@ export async function withdawBalance(user: any, amount: number)
 			{
 				type: TransactionType.WITHDRAWAL,
 				amount: amount,
-				userId: user.Id
+				userId: user.Id,
+				status: TransactionStatus.SUCCESS
 			});
 	
 			const updatedBalance = user.balance - amount;
@@ -135,6 +152,56 @@ export async function withdawBalance(user: any, amount: number)
 		}
 		else
 		{
+			await createTransaction(
+			{
+				type: TransactionType.WITHDRAWAL,
+				amount: amount,
+				userId: user.Id,
+				status: TransactionStatus.FAIL
+			});
+
+			return undefined;
+		}
+	}
+	catch (e)
+	{
+		return undefined;
+	}
+}
+
+export async function payFee(user: any, amount: number)
+	: Promise<number | undefined>
+{
+	try
+	{
+		if (amount <= user.balance)
+		{
+			await createTransaction(
+			{
+				type: TransactionType.FEE,
+				amount: amount,
+				userId: user.Id,
+				status: TransactionStatus.SUCCESS
+			});
+	
+			const updatedBalance = user.balance - amount;
+
+			if (updatedBalance < user.withdrawAmount)
+				await updateWithdrawabalBalance(user, updatedBalance);
+			await updateBalance(user, updatedBalance);
+
+			return updatedBalance;
+		}
+		else
+		{
+			await createTransaction(
+			{
+				type: TransactionType.FEE,
+				amount: amount,
+				userId: user.Id,
+				status: TransactionStatus.FAIL
+			});
+
 			return undefined;
 		}
 	}
