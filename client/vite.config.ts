ac9d22837bcb67fadad4a210c780d5a266d57442
MatWaze <coolmartun@gmail.com>
2025-11-07 21:49:31 +0400

    pointless

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,5 @@ export default defineConfig({
   server: {
     port: 3065,
     open: './client.html'
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './client.html',
-        login: './login.html',
-        register: './register.html'
-      }
-    }
-  },
+  }
 });
