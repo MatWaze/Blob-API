21d3365cf9741d0fb3446d7e71bab47ea8450c35
MatWaze <coolmartun@gmail.com>
2025-11-07 16:34:35 +0400

    not including confirmPassword in here

--- api/repositories/userRepository/IUserRepository.ts
+++ api/repositories/userRepository/IUserRepository.ts
@@ -6,7 +6,13 @@ export interface IUserRepository
 	getUserByEmailAsync(email: string) : Promise<any>;
 	getUserByNonceAsync(nonce: string) : Promise<any>;
 	confirmEmailAsync(userId: string) : Promise<void>;
-	createUserAsync(user: CreateUserType) : Promise<any>;
+	createUserAsync(user:
+	{
+		email: string;
+		username: string;
+		password: string;
+		authMethod: string;
+	}) : Promise<any>;
 	// updateUserAsync(user: any) : Promise<any>;
 	deleteUserAsync(id: string) : Promise<void>;
 	updateBalanceAsync(userId: string, amount: number) : Promise<void>;
